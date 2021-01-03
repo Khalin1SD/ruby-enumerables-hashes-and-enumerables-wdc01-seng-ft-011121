d4d9923bcf5c0cf228bd1e918ffed6c6a2fc8390
calm-collection-3916 <khalinredd@gmail.com>
2021-01-03 19:49:44 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,17 +8,16 @@
 
 def select_winner(passengers)
   winner = " "
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name 
-    end
-  end
-end
-passengers = { 
+  passengers = { 
   suite_a: "Amanda Presley", 
   suite_b: "Seymour Hoffman", 
   suite_c: "Alfred Tennyson", 
   suite_d: "Charlie Chaplin", 
   suite_e: "Crumpet the Elf"
 }
-select_winner(passengers)
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name 
+    end
+  end
+end
