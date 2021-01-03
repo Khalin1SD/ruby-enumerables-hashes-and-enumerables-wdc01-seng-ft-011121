7048f6c06d8bfe790d3fcbcd9cbb2f060214c525
calm-collection-3916 <khalinredd@gmail.com>
2021-01-03 19:48:41 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,3 +14,11 @@ def select_winner(passengers)
     end
   end
 end
+passengers = { 
+  suite_a: "Amanda Presley", 
+  suite_b: "Seymour Hoffman", 
+  suite_c: "Alfred Tennyson", 
+  suite_d: "Charlie Chaplin", 
+  suite_e: "Crumpet the Elf"
+}
+select_winner(passengers)
